f2a376c0228eb27ef0de35bc4512d012edad2d00
webdevrob <79129199+equuleos@users.noreply.github.com>
2024-01-03 14:43:20 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -31,3 +31,19 @@
   })(window.jQuery);
 
 
+//sliders
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+  }
+
