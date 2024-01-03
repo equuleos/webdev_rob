97c8fc9b47d4eab38e0e02ec34d879c4402bfd5e
webdevrob <79129199+equuleos@users.noreply.github.com>
2024-01-04 00:28:48 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -32,18 +32,25 @@
 
 
 //sliders
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides("seoSlider");
+  showSlides("wordpressSlider");
+  showSlides("shopifySlider");
+});
+
+function showSlides(sliderId) {
+  let currentSlide = 0;
+  let slides = document.getElementById(sliderId).getElementsByClassName("mySlides");
+
+  function displaySlide() {
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].style.display = "block";
+      setTimeout(displaySlide, 2000); // Adjust the time interval as needed
   }
+
+  displaySlide();
+}
 
